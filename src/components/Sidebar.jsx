5afe746b8ec5f083086298140d7ec209bf2f2c90
Helton Pereira <helton.bfsm@gmail.com>
2022-08-05 18:41:42 -0300

    push na branch req 4 agora vai

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import { getCategories } from '../services/api';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        Categorias
+        <nav>
+          <button type="button">x</button>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
