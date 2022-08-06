fd68bd65ab0587d4fc69c21a4241bfb776429c00
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-08-05 21:29:37 -0300

    Requisito 4: Liste as categorias de produtos disponíveis via API na página principal

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-// import { getCategories } from '../services/api';
-
+import { getCategories } from '../services/api';
+// REQUISITO 4
 class Sidebar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const categoriesObject = await getCategories();
+
+    this.setState({ categories: categoriesObject });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
-        Categorias
+        Categorias:
         <nav>
-          <button type="button">Informática</button>
+          {categories.map((categorie) => (
+            <button type="button" data-testid="category" key={ categorie.id }>
+              {categorie.name}
+            </button>
+          ))}
         </nav>
       </div>
     );
