777a8f46959bde288888179cb79aa16319d98a1b
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-08-09 19:00:09 -0300

    Requisito 11: finalmentegit add .!

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 // import { Link } from 'react-router-dom';
 import { getProductById } from '../services/api';
+import AddReview from '../components/AddReview';
 
 export default class Product extends Component {
   constructor() {
@@ -25,13 +26,16 @@ export default class Product extends Component {
     const { title, thumbnail, price } = product;
 
     return (
-      <div>
-        <h1>TESTE</h1>
-        <Header />
-        <h1 data-testid="product-detail-name">{ title }</h1>
-        <img src={ thumbnail } alt={ title } data-testid="product-detail-image" />
-        <span data-testid="product-detail-price">{ price }</span>
-      </div>
+      <section>
+        <div>
+          <h1>TESTE</h1>
+          <Header />
+          <h1 data-testid="product-detail-name">{ title }</h1>
+          <img src={ thumbnail } alt={ title } data-testid="product-detail-image" />
+          <span data-testid="product-detail-price">{ price }</span>
+        </div>
+        <AddReview />
+      </section>
     );
   }
 }
